cefde145981c33be776ed85b5d346bafa8fdb610
Gaetano Giunta <gg@ez.no>
2010-02-19 16:39:56 +0000

    - change quote style to better interact with build tool
    git-svn-id: http://svn.projects.ez.no/ezlightbox/trunk@11 f3dee671-0d9b-48e2-8c34-da7cf695c913

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class ezlightboxInfo
     static function info()
     {
         return array( 'Name'      => "eZ Publish lightbox",
-                      'Version'   => "0.8.0-dev",
+                      'Version'   => '0.8.0-dev',
                       'Copyright' => "Copyright (C) 1999-2009 eZ Systems AS",
                       'License'   => "GNU General Public License v2.0"
                     );
