4e613032a59f68948712eee91e6cc35fb8c0b8e2
Paul Borgermans <pb@ez.no>
2010-02-22 13:55:59 +0000

    - preparing build of 1.0-beta1, changing meta-data
    git-svn-id: http://svn.projects.ez.no/ezlightbox/trunk@12 f3dee671-0d9b-48e2-8c34-da7cf695c913

--- ezinfo.php
+++ ezinfo.php
@@ -6,7 +6,7 @@ class ezlightboxInfo
     {
         return array( 'Name'      => "eZ Publish lightbox",
                       'Version'   => '0.8.0-dev',
-                      'Copyright' => "Copyright (C) 1999-2009 eZ Systems AS",
+                      'Copyright' => "Copyright (C) 1999-2010 eZ Systems AS",
                       'License'   => "GNU General Public License v2.0"
                     );
     }
