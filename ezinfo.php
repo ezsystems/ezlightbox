a6753a2f741ac548fa17ac882462b2942936172c
Paul Borgermans <pb@ez.no>
2010-03-26 15:39:52 +0000

    - updating revision number
    git-svn-id: http://svn.projects.ez.no/ezlightbox/stable/0.7@32 f3dee671-0d9b-48e2-8c34-da7cf695c913

--- ezinfo.php
+++ ezinfo.php
@@ -28,7 +28,7 @@ class ezlightboxInfo
     static function info()
     {
         return array( 'Name'      => "eZ Publish lightbox",
-                      'Version'   => "0.7.6",
+                      'Version'   => "0.7.7",
                       'Copyright' => "Copyright (C) 1999-2009 eZ Systems AS",
                       'License'   => "GNU General Public License v2.0"
                     );
