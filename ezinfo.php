d601506bfdb96523a33f8f7ea427f7cf674468e9
Bertrand Dunogier <bd@ez.no>
2011-02-19 18:05:20 +0100

    Updated ezinfo.php & extension.xml with proper version number tag

--- ezinfo.php
+++ ezinfo.php
@@ -28,9 +28,9 @@ class ezlightboxInfo
     static function info()
     {
         return array( 'Name'      => "eZ Publish lightbox",
-                      'Version'   => '0.8.0 RC2',
+                      'Version'   => '//autogentag//',
                       'Copyright' => "Copyright (C) 1999-2010 eZ Systems AS",
-                      'License'   => "GNU General Public License v2.0"
+                      'License'   => "//EZP_LICENSE//",
                     );
     }
 }
