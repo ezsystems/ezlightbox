0df7fe4e0f967a1492b1605b62431925f686a5f6
niedziella <d.niedziella@circit.de>
2012-06-29 10:34:48 +0200

    - update version information

--- ezinfo.php
+++ ezinfo.php
@@ -28,7 +28,7 @@ class ezlightboxInfo
     static function info()
     {
         return array( 'Name'      => "eZ Publish lightbox",
-                      'Version'   => "0.7.7",
+                      'Version'   => "//autogentag//",
                       'Copyright' => "Copyright (C) 1999-2012 eZ Systems AS",
                       'License'   => "GNU General Public License v2.0"
                     );
